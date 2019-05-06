fe5ef736714641f5f115e933b803656418b780a8
hellfellow <49310546+hellfellow@users.noreply.github.com>
2019-05-06 14:50:21 +0300

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,34 @@
+﻿window.onscroll=()=>{
+const navbar=document.querySelector('header')
+if (window.pageYOffset !== 0){navbar.style.boxShadow = '0 0 10px black';}
+else {navbar.style.boxShadow = '';}}
+
+
+$( ".skill" ).click(function() {
+  $( ".skill" ).animate({ "left": "+=50px" }, "fast" );
+});
+
+
+
+/*
+window.onscroll=()=>{
+if (window.pageYOffset !== 0){ 
+$( ".photo" ).animate({ "top": "+=50px" }, "fast" );};};
+*/
+
+
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
